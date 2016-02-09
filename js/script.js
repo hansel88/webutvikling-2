046171f5930bf868b1150ad527db1795d8d06e3d
Hans Petter Naumann <hans.petter.naumann@sigmagroup.no>
2016-02-09 12:57:36 +0100

    jej

--- js/script.js
+++ js/script.js
@@ -1,24 +1,104 @@
 (function () {
+    
+    'use strict';
 
-var victims = { list: {
-  "1" : {
-    "city"        : "Oslo",
-    "Born"        : "1992",
-    "Age"         : "18",
-    "Description" : "Hun var en solstråle.",
-    "image"       : "images/victims/test.jpg"
+var victims = { list: [
+  {
+    "name"        : "Anne Lise Holter",
+    "city"        : "Våler, Østfold",
+    "born"        : "",
+    "age"         : "51",
+    "description" : "Anne Lise Holter fra Våler i Østfold var seniorkonsulent ved Statsministerens kontor, og var på jobb da bomben gikk av. Hun etterlater seg"                 + "ektemann og tre barn.Holter ble funnet død i resepsjonsområdet til Høyblokka i Regjeringskvartalet.",
+    "image"       : "../images/victims/anne-lise-holter.jpg",
+    "found"       : "Regjeringskvartalet"
   },
-  "2" : {
+  {
+    "name"        : "Hanna M. Orvik Endresen",
     "city"        : "Oslo",
-    "Born"        : "1992",
-    "Age"         : "18",
-    "Description" : "Hun var en solstråle.",
-    "image"       : "images/victims/test.jpg"
+    "born"        : "",
+    "age"         : "61",
+    "description" : "Hanna Orvik Endresen var på jobb i Høyblokka. Hun arbeidet som resepsjonsvakt i sikkerhetsavdelingen i Departementenes servicesenter. Hun"                    + "blir beskrevet som en god kollega, som vil bli sterkt savnet. Endresen døde på jobb i Høyblokka i Regjeringskvartalet.",
+    "image"       : "../images/victims/test.jpg",
+    "found"       : "Regjeringskvartalet"
   },
-}};
+  {
+    "name"        : "Hanne Ekroll Løvlie",
+    "city"        : "Oslo",
+    "born"        : "",
+    "age"         : "30",
+    "description" : "Hanne Løvlie fra Tyristrand på Ringerike, bosatt i Oslo, var på jobb i Regjeringskvartalet da bomben smalt. Hun var utdannet statsviter, og"                 + " jobbet som førstekonsulent i Arbeidsdepartementet. Hun blir beskrevet som en meget dyktig medarbeider og et ungt, flott menneske av"                       + " arbeidsminister Hanne Bjurstrøm. Løvlie var utenfor inngangen til Høyblokka i Regjeringskvartalet." ,
+    "image"       : "../images/victims/anne-lise-holter.jpg",
+    "found"       : "Regjeringskvartalet"
+  },
+  {
+    "name"        : "Ida Marie Hill",
+    "city"        : "Oslo",
+    "born"        : "",
+    "age"         : "34",
+    "description" : "Ida Marie Hill omkom da bomben i Oslo gikk av. Hun jobbet som rådgiver for Justisdepartementet. Hun er opprinnelig fra Grue, men har bodd i"                  + " Oslo i mange år.Hill var ved inngangen til Høyblokka i Regjeringskvartalet.",
+    "image"       : "../images/victims/test.jpg",
+    "found"       : "Regjeringskvartalet"
+  },
+  {
+    "name"        : "Jon Vegard Lervåg",
+    "city"        : "Oslo",
+    "born"        : "",
+    "age"         : "32",
+    "description" : "Jon Vegard Lervåg hadde kun jobbet i Justisdepartementet i ett års tid da bomben smalt. Juristen utmerket seg som en samfunnsengasjert"                     + " person. Han var gift, men hadde ikke barn.Lervåg var ved inngangen til Høyblokka i Regjeringskvartalet." ,
+    "image"       : "../images/victims/anne-lise-holter.jpg",
+    "found"       : "Regjeringskvartalet"
+  },
+  {
+    "name"        : "Kai Hauge",
+    "city"        : "Oslo",
+    "born"        : "",
+    "age"         : "32",
+    "description" : "Kai Hauge var bosatt i Oslo, men født og oppvokst i Stavanger. De siste tre årene har han arbeidet i utelivsbransjen i Oslo, og startet i"                 + " februar 2011 sitt eget utestedskonsept «Blå Resept Bar & Sånt» i Oslo sentrum. Hauge var i Grubbegata i Regjeringskvartalet.",
+    "image"       : "../images/victims/test.jpg",
+    "found"       : "Regjeringskvartalet"
+  },
+  {
+    "name"        : "Kjersti Berg Sand",
+    "city"        : "Oslo",
+    "born"        : "",
+    "age"         : "26",
+    "description" : "Kjersti Berg Sand omkom da bomben i Oslo gikk av. Hun jobbet i Justisdepartementets sivilavdeling. Hun var opprinnelig fra Nord-Odal, men"                  + " var bosatt i Oslo. Sand befant seg i resepsjonsområdet i Høyblokka i Regjeringskvartalet." ,
+    "image"       : "../images/victims/anne-lise-holter.jpg",
+    "found"       : "Regjeringskvartalet"
+  },
+  {
+    "name"        : "Tove Åshill Knutsen",
+    "city"        : "Oslo",
+    "born"        : "",
+    "age"         : "56",
+    "description" : "Tove Åshill Knutsen omkom i bombeangrepet mot regjeringskvartalet da hun var på vei til T-banen. Hun var administrasjonssekretær ved"                       + " forbundskontoret i El og IT-Forbundet, og blir omtalt som en blid kvinne som var godt likt av alle."
+                    + "Knutsen var i Grubbegata ved Einar Gerhardsens plass i Regjeringskvartalet.",
+    "image"       : "../images/victims/test.jpg",
+    "found"       : "Regjeringskvartalet"
+  },
+  {
+    "name"        : "Aleksander Aas Eriksen",
+    "city"        : "Meråker",
+    "born"        : "",
+    "age"         : "16",
+    "description" : "Aleksander Aas Eriksen var på Utøya som leder for Stjørdal AUF. Han var akkurat ferdig med sitt første år ved Ole Vig videregående skole i"                   + " Stjørdal ved helse- og sosiallinjen. Han betegnes som en svært engasjert ungdom. Han var i Kafébygget på Utøya." ,
+    "image"       : "../images/victims/anne-lise-holter.jpg",
+    "found"       : "Utøya"
+  },
+  {
+    "name"        : "Anders Kristiansen",
+    "city"        : "Bardu",
+    "born"        : "",
+    "age"         : "18",
+    "description" : "Anders Kristiansen fra Bardu var leder i Bardu AUF, som han selv tok initiativ til å starte. Han ledet også Ungdommens fylkesråd i Troms og"                 + " beskrives av sentrale politikere som et stort politisk talent. Han var på Kjærlighetsstien på Utøya.",
+    "image"       : "../images/victims/test.jpg",
+    "found"       : "Utøya"
+  },
+]};
 
 /*$( "#target" ).click(function() {
   $("#mapUtoya")[0].scrollIntoView({block: "end", behavior: "smooth"});
 });*/
+    console.log(victims);
 
 })();
