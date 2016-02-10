a2a3e4327c8f3fef622f1bd05ef8c67bd09135db
Hans Petter Naumann <hans.petter.naumann@sigmagroup.no>
2016-02-10 19:06:05 +0100

    .

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@
         
         $( ".marker" ).each(function( index ) {
             $( this ).mouseenter(function() {
+                
                $(this).next().css({"opacity" : 1});
             });
           
@@ -19,16 +20,34 @@
         
     }
     
+    setEvents();
+    
     var updateMap = function(){
+        updateAreas();
+    }
+    
+    var updateAreas = function(){
+        
+        var fromAge = 16; //Get actual value
+        var toAge = 30; //Get actual value
+        var location = 'utoya'; //Get actualy value
         
         $( ".marker" ).each(function( index ) {
+            var count = 0;
             var area = $( this ).attr('id');
             for(var i = 0; i < victims.list.length; i++ ){
-                if()
+                if(fromAge <= victims.list[i].age && toAge >= victims.list[i].age){
+                    if(true)//Check utøya/regjeringskvaralter
+                    {
+                        count++;
+                    }
+                }
             }
+            console.log(count);
+            $( this ).css({"width" : (5 + (2 * count)) + "px", "height" : (5 + (2 * count)) + "px"});
         });
     }
     
-    
+    updateMap();
 
 })();
