cbb1cf652dc73126fa91828817fe4b130c01ad0e
Hans Petter Naumann <hans.petter.naumann@sigmagroup.no>
2016-02-10 17:41:36 +0100

    .

--- js/script.js
+++ js/script.js
@@ -3,5 +3,32 @@
     'use strict';
 
     console.log(victims);
+    
+    var setEvents = function(){
+        
+        $( ".marker" ).each(function( index ) {
+            $( this ).mouseenter(function() {
+               $(this).next().css({"opacity" : 1});
+            });
+          
+
+            $( this ).mouseleave(function() {
+               $(this).next().css({"opacity" : 0});
+            });
+        });
+        
+    }
+    
+    var updateMap = function(){
+        
+        $( ".marker" ).each(function( index ) {
+            var area = $( this ).attr('id');
+            for(var i = 0; i < victims.list.length; i++ ){
+                if()
+            }
+        });
+    }
+    
+    
 
 })();
--- js/victims.js
+++ js/victims.js
@@ -1,11 +1,9 @@
-(function () {
-    
-    'use strict';
 
 var victims = { list: [
   {
     "name"        : "Anne Lise Holter",
-    "city"        : "Våler, Østfold",
+    "city"        : "Våler",
+    "area"        : "Ostfold",
     "born"        : "",
     "age"         : "51",
     "description" : "Anne Lise Holter fra Våler i Østfold var seniorkonsulent ved Statsministerens kontor, og var på jobb da bomben gikk av. Hun etterlater seg"                 + "ektemann og tre barn.Holter ble funnet død i resepsjonsområdet til Høyblokka i Regjeringskvartalet.",
@@ -15,6 +13,7 @@ var victims = { list: [
   {
     "name"        : "Hanna M. Orvik Endresen",
     "city"        : "Oslo",
+    "area"        : "Oslo",
     "born"        : "",
     "age"         : "61",
     "description" : "Hanna Orvik Endresen var på jobb i Høyblokka. Hun arbeidet som resepsjonsvakt i sikkerhetsavdelingen i Departementenes servicesenter. Hun"                    + "blir beskrevet som en god kollega, som vil bli sterkt savnet. Endresen døde på jobb i Høyblokka i Regjeringskvartalet.",
@@ -24,6 +23,7 @@ var victims = { list: [
   {
     "name"        : "Hanne Ekroll Løvlie",
     "city"        : "Oslo",
+    "area"        : "Oslo",
     "born"        : "",
     "age"         : "30",
     "description" : "Hanne Løvlie fra Tyristrand på Ringerike, bosatt i Oslo, var på jobb i Regjeringskvartalet da bomben smalt. Hun var utdannet statsviter, og"                 + " jobbet som førstekonsulent i Arbeidsdepartementet. Hun blir beskrevet som en meget dyktig medarbeider og et ungt, flott menneske av"                     + " arbeidsminister Hanne Bjurstrøm. Løvlie var utenfor inngangen til Høyblokka i Regjeringskvartalet." ,
@@ -33,6 +33,7 @@ var victims = { list: [
   {
     "name"        : "Ida Marie Hill",
     "city"        : "Oslo",
+    "area"        : "Oslo",
     "born"        : "",
     "age"         : "34",
     "description" : "Ida Marie Hill omkom da bomben i Oslo gikk av. Hun jobbet som rådgiver for Justisdepartementet. Hun er opprinnelig fra Grue, men har bodd i"                  + " Oslo i mange år.Hill var ved inngangen til Høyblokka i Regjeringskvartalet.",
@@ -42,6 +43,7 @@ var victims = { list: [
   {
     "name"        : "Jon Vegard Lervåg",
     "city"        : "Oslo",
+    "area"        : "Oslo",
     "born"        : "",
     "age"         : "32",
     "description" : "Jon Vegard Lervåg hadde kun jobbet i Justisdepartementet i ett års tid da bomben smalt. Juristen utmerket seg som en samfunnsengasjert"                     + " person. Han var gift, men hadde ikke barn.Lervåg var ved inngangen til Høyblokka i Regjeringskvartalet." ,
@@ -51,6 +53,7 @@ var victims = { list: [
   {
     "name"        : "Kai Hauge",
     "city"        : "Oslo",
+    "area"        : "Oslo",
     "born"        : "",
     "age"         : "32",
     "description" : "Kai Hauge var bosatt i Oslo, men født og oppvokst i Stavanger. De siste tre årene har han arbeidet i utelivsbransjen i Oslo, og startet i"                   + " februar 2011 sitt eget utestedskonsept «Blå Resept Bar & Sånt» i Oslo sentrum. Hauge var i Grubbegata i Regjeringskvartalet.",
@@ -60,6 +63,7 @@ var victims = { list: [
   {
     "name"        : "Kjersti Berg Sand",
     "city"        : "Oslo",
+    "area"        : "Oslo",
     "born"        : "",
     "age"         : "26",
     "description" : "Kjersti Berg Sand omkom da bomben i Oslo gikk av. Hun jobbet i Justisdepartementets sivilavdeling. Hun var opprinnelig fra Nord-Odal, men"                   + " var bosatt i Oslo. Sand befant seg i resepsjonsområdet i Høyblokka i Regjeringskvartalet." ,
@@ -69,6 +73,7 @@ var victims = { list: [
   {
     "name"        : "Tove Åshill Knutsen",
     "city"        : "Oslo",
+    "area"        : "Oslo",
     "born"        : "",
     "age"         : "56",
     "description" : "Tove Åshill Knutsen omkom i bombeangrepet mot regjeringskvartalet da hun var på vei til T-banen. Hun var administrasjonssekretær ved"                       + " forbundskontoret i El og IT-Forbundet, og blir omtalt som en blid kvinne som var godt likt av alle."
@@ -79,6 +84,7 @@ var victims = { list: [
   {
     "name"        : "Aleksander Aas Eriksen",
     "city"        : "Meråker",
+    "area"        : "Nord-Trondelag",
     "born"        : "",
     "age"         : "16",
     "description" : "Aleksander Aas Eriksen var på Utøya som leder for Stjørdal AUF. Han var akkurat ferdig med sitt første år ved Ole Vig videregående skole i"                  + " Stjørdal ved helse- og sosiallinjen. Han betegnes som en svært engasjert ungdom. Han var i Kafébygget på Utøya." ,
@@ -88,6 +94,7 @@ var victims = { list: [
   {
     "name"        : "Anders Kristiansen",
     "city"        : "Bardu",
+    "area"        : "Troms",
     "born"        : "",
     "age"         : "18",
     "description" : "Anders Kristiansen fra Bardu var leder i Bardu AUF, som han selv tok initiativ til å starte. Han ledet også Ungdommens fylkesråd i Troms og"                 + " beskrives av sentrale politikere som et stort politisk talent. Han var på Kjærlighetsstien på Utøya.",
@@ -97,6 +104,7 @@ var victims = { list: [
   {
     "name"        : "Andreas Dalby Grønnesby",
     "city"        : "Stange",
+    "area"        : "Hedmark",
     "born"        : "",
     "age"         : "17",
     "description" : "Andreas Dalby var på Utøya for første gang. Han gikk på Hamar katedralskole."
@@ -107,6 +115,7 @@ var victims = { list: [
   {
     "name"        : "Andreas Edvardsen",
     "city"        : "Sarpsborg",
+    "area"        : "Ostfold",
     "born"        : "",
     "age"         : "18",
     "description" : "Andreas Edvardsen var styremedlem i Sarpsborg AUF og sosialpolitisk ansvarlig i AUFs fylkesstyre i Østfold. Han var også kandidat på"                       + " Sarpsborg Arbeiderpartis liste til kommunevalget. I mai var han russ ved St. Olav videregående skole i Sarpsborg.Han var på "                             + " Kjærlighetsstien på Utøya.",
@@ -115,9 +124,3 @@ var victims = { list: [
   },
 ]};
 
-/*$( "#target" ).click(function() {
-  $("#mapUtoya")[0].scrollIntoView({block: "end", behavior: "smooth"});
-});*/
-    console.log(victims);
-
-})();
