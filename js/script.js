8bb12fb4269280efbf34be4f43b77402e625a0d0
hansel88 <hpn_x@hotmail.com>
2016-02-15 19:09:12 +0100

    stasj

--- js/script.js
+++ js/script.js
@@ -5,6 +5,29 @@
     var fromAge = 14;
     var toAge = 70;
 
+    //This is only needed if infoText should not take filtering into account 
+    var areas = [
+        {"area": "Finnmark", "tag": "finnmark", "dead-count": 1},
+        {"area": "Troms", "tag": "troms", "dead-count": 1},
+        {"area": "Nord-trøndelag", "tag": "nord-trondelag", "dead-count": 1},
+        {"area": "Sør-trøndelag", "tag": "sor-trondelag", "dead-count": 1},
+        {"area": "Møre og Romsdal", "tag": "more-og-romsdal", "dead-count": 1},
+        {"area": "Hedmark", "tag": "hedmark", "dead-count": 1},
+        {"area": "Oppland", "tag": "oppland", "dead-count": 1},
+        {"area": "Sogn og fjordane", "tag": "sogn-og-fjordane", "dead-count": 1},
+        {"area": "Hordaland", "tag": "hordaland", "dead-count": 1},
+        {"area": "Akershus", "tag": "akershus", "dead-count": 1},
+        {"area": "Østfold", "tag": "ostfold", "dead-count": 1},
+        {"area": "Buskerud", "tag": "finnmark", "dead-count": 1},
+        {"area": "Finnmark", "tag": "finnmark", "dead-count": 1},
+        {"area": "Finnmark", "tag": "finnmark", "dead-count": 1},
+        {"area": "Finnmark", "tag": "finnmark", "dead-count": 1},
+        {"area": "Finnmark", "tag": "finnmark", "dead-count": 1},
+        {"area": "Finnmark", "tag": "finnmark", "dead-count": 1},
+        {"area": "Finnmark", "tag": "finnmark", "dead-count": 1},
+        {"area": "Finnmark", "tag": "finnmark", "dead-count": 1}
+    ]
+
     console.log(victims);
     
     //Setting up age slider
@@ -33,9 +56,14 @@
                 fromAge = data.values.min;
                 toAge = data.values.max;
                 updateAreas();
+                $('#ageSliderText').html(fromAge + ' - ' + toAge + ' år');
             });
             
-            $('#locationFilter').off().on('change', function (e) {
+            $('#osloCheckbox').off().on('change', function (e) {
+                updateAreas();
+            });
+
+            $('#utoyaCheckbox').off().on('change', function (e) {
                 updateAreas();
             });
 
@@ -46,24 +74,23 @@
     setEvents();
     
     var updateAreas = function(){
-        var location = $('#locationFilter').val();
+        var includeUtoya = $('#utoyaCheckbox').is(':checked');
+        var includeOslo = $('#osloCheckbox').is(':checked'); 
         
         $( ".marker" ).each(function( index ) {
             var count = 0;
             var area = $( this ).attr('id');
-            console.log(area);
             for(var i = 0; i < victims.list.length; i++ ){
                 if(area.toLowerCase().indexOf(victims.list[i].area) > -1){
                     if(fromAge <= victims.list[i].age && toAge >= victims.list[i].age){ //Checking age
-                        if(location == 'all' || (location == victims.list[i].found)) //Check utøya/regjeringskvartalet
+                        if((includeOslo && ('oslo' == victims.list[i].found)) || (includeUtoya && ('utoya' == victims.list[i].found))) //Check utøya/regjeringskvartalet
                         {
                                 count++;
                         }
                     }
                 }
             }
-            console.log(count);
-            $( this ).animate({"width" : (6 + (2 * count)) + "px", "height" : (6 + (2 * count)) + "px"}, 300);
+            $( this ).animate({"width" : (6 + (2 * count)) + "px", "height" : (6 + (2 * count)) + "px"}, 500);
         });
     }
     
